69459be43b6536cb80ea2c8c619dc369aeff863c
Gexiaowei <gandxiaowei@gmail.com>
2025-03-06 11:47:58 +0800

    Initial Commit

---
+++ day.js/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  day.js
+//
+//  Created by 戈晓伟 on 2025/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ day.js/day_jsApp.swift
@@ -0,0 +1,17 @@
+//
+//  day_jsApp.swift
+//  day.js
+//
+//  Created by 戈晓伟 on 2025/3/6.
+//
+
+import SwiftUI
+
+@main
+struct day_jsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
